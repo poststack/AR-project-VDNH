3afbb8e4c0ff68cbc9cc3f6a8b70f67fdfaa713e
poststack <dundukpost@gmail.com>
2022-11-06 22:20:12 +0300

    First COMmit
    1

---
+++ Assets/StartupScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+
+ 
+
+public class StartupScript : MonoBehaviour
+{
+	
+	public Vector2 newPos;
+	
+
+    // Start is called before the first frame update
+    void Start()
+	{
+		Sequence mySequence = DOTween.Sequence();
+		mySequence.Append(GetComponent<RectTransform>().DOAnchorPos(newPos ,  4).SetEase(Ease.InOutBounce));
+		mySequence.PrependInterval(4);
+		mySequence.Play();	 
+	}
+    
+	public void Button()
+	{
+		DOTween.PauseAll();
+		GetComponent<RectTransform>().DOAnchorPos(newPos ,  4).SetEase(Ease.InOutFlash);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
